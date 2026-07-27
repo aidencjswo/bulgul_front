def12d0d54f45703f7f833bc18850357ca728f43
aiden_mac <aidencjswo@gmail.com>
2026-07-27 22:51:21 +0900

    Initial Commit

---
+++ bulgul_front/bulgul_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  bulgul_frontApp.swift
+//  bulgul_front
+//
+//  Created by 안상영 on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct bulgul_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
